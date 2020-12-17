7b2c33706b14556e5419ac6ebecf897d2c1e98bd
Anastassios Nanos <ananos@nubificus.co.uk>
2020-12-17 11:12:28 +0000

    Update create_rootfs.sh

--- guest/create_rootfs.sh
+++ guest/create_rootfs.sh
@@ -3,7 +3,7 @@
 dd if=/dev/zero of=rootfs.img bs=1M count=0 seek=512
 mkfs.ext4 rootfs.img
 mkdir -p mnt
-mount rootfs.img mnt
-rsync -aogxvPH $@ mnt
-umount mnt
+sudo mount rootfs.img mnt
+sudo rsync -aogxvPH $@ mnt
+sudo umount mnt
 rmdir mnt
