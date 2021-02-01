7474e0f240dd48c60348dfeee47a9ad5613fc7d9
Charalampos Mainas <charalampos.mainas@gmail.com>
2021-02-01 19:26:29 +0200

    Update unikraft related Dockerfiles
    Unikraft supports vaccelrt, so we need to udaye the Dockerfiles so they
    can reflect the changes. From now on we use vaccelrt for both the host and
    the unikraft applications.
    
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build --network=host -t nubificus/unikernels_vaccel-qemu --build-arg "TOKEN=$TOKEN" .
+usage()
+{ 
+	echo "Build the host where unikernel guest using vAccel can be executed"
+	echo "usage:"
+	echo "	-h	Display this help message"
+	echo "	-u	Build host with vAccelrt support"
+	echo "	-r	Build host without vAccelrt support"
+	exit 1
+}
+
+while getopts "hur" flag
+do
+    case "${flag}" in
+        h)
+		usage
+	;;
+        u)
+		unikernel=unikraft
+		DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile_rt -t nubificus/unikernels_vaccel-qemu --build-arg "TOKEN=$TOKEN" .
+	;;
+        r)
+		unikernel=rumprun
+		DOCKER_BUILDKIT=1 docker build --network=host -t nubificus/unikernels_vaccel-qemu --build-arg "TOKEN=$TOKEN" .
+	;;
+	*)
+		usage
+	;;
+    esac
+done
+
+shift $((OPTIND-1))
+if [ -z "${unikernel}" ]; then
+	usage
+fi
