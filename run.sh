f5682d56d3560edfb2b48a2529763fed09e0ab5f
Charalampos Mainas <charalampos.mainas@gmail.com>
2021-02-04 23:48:08 +0200

    Changes on building guest and in sharing data with container
    Unikraft and the application are now seperated repos, so we needed to
    take account of that.
    
    Also it is better to mount guest data directory when running the
    container so we can easily add new images without having to build any
    container again.
    
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ##docker run --rm --network=host --device=/dev/net/tun --cap-add NET_ADMIN --gpus all -v $(pwd)/guest/qemu-rumprun-x86_64:/data -it --device=/dev/kvm nubificus/unikernels_vaccel-qemu
-docker run --rm --network=host --device=/dev/net/tun --cap-add NET_ADMIN --gpus all -it --device=/dev/kvm nubificus/unikernels_vaccel-qemu bash /data/qemu_run.sh $1 $2
+docker run --rm --network=host --device=/dev/net/tun --cap-add NET_ADMIN --gpus all -v $(pwd)/guest/qemu-guest-x86_64:/data -it --device=/dev/kvm nubificus/unikernels_vaccel-qemu bash /data/qemu_run.sh $1 $2
