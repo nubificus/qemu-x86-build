c7ede8643ded54609729d0e03af98221370d835e
Charalampos Mainas <cmainas@nubificus.co.uk>
2023-01-31 18:39:28 +0000

    Build guest through the repo's Dockerfile
    The new repo with all the examples for vAccel in Unikraft contains a
    Dockerfile which can build the specified example. Replace the Dockerfile
    in this repo with the aformentioned one.
    
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- build_guest.sh
+++ build_guest.sh
@@ -4,13 +4,13 @@ usage()
 { 
 	echo "Build a unikernel image and get everything ready to run this unikernel:"
 	echo "usage:"
-	echo "	-h	Display this help message"
-	echo "	-u	Build unikraft unikernel"
-	echo "	-r	Build rumprun unikernel"
+	echo "	-h		Display this help message"
+	echo "	-u <example>	Build unikraft unikernel"
+	echo "	-r		Build rumprun unikernel"
 	exit 1
 }
 
-while getopts "hur" flag
+while getopts "hu:r" flag
 do
     case "${flag}" in
         h)
@@ -18,12 +18,14 @@ do
 	;;
         u)
 		unikernel=unikraft
+		example=$OPTARG
 	;;
         r)
 		unikernel=rumprun
 	;;
 	*)
 		usage
+		exit 1
 	;;
     esac
 done
@@ -31,10 +33,24 @@ done
 shift $((OPTIND-1))
 if [ -z "${unikernel}" ]; then
 	usage
+	exit 1
 fi
 
-cd guest
-
-DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile_${unikernel}.x86_64 -t nubificus/vaccel-qemu_${unikernel} --build-arg "TOKEN=$TOKEN" --target artifacts --output type=local,dest=./qemu-guest-x86_64 .
-
-cp -r data ${unikernel}_example/qemu_run.sh ./qemu-guest-x86_64
+if [[ "$unikernel" == "rumprun" ]]; then
+	cd guest
+	
+	sudo DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile_rumprun.x86_64 -t nubificus/vaccel-qemu_rumprun --build-arg "TOKEN=$TOKEN" --target artifacts --output type=local,dest=./qemu-guest-x86_64 .
+	
+	cp -r data ${unikernel}_example/qemu_run.sh ./qemu-guest-x86_64
+elif [[ "$unikernel" == "unikraft" ]]; then
+	if [ -z "${example}" ]; then
+		usage
+		exit 1
+	fi
+	pushd guest/
+	pushd unikraft_example
+	sudo DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile -t unikraft_vaccel_example --build-arg "EXAMPLE=$example" --target artifacts --output type=local,dest=../qemu-guest-x86-64 .
+	popd
+	sudo cp -r data unikraft_example/qemu_run.sh ./qemu-guest-x86-64
+	popd
+fi
